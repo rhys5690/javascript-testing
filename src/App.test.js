00b4ca5fcd4d6995fca22803defc42319bef7e0a
rhys5690 <rhys5690@gmail.com>
2018-09-10 09:47:15 +1000

    tested external dependency

--- src/App.test.js
+++ src/App.test.js
@@ -9,4 +9,6 @@ jest.mock("./add", () => ({
 test("total", () => {
   expect(total(5, 20)).toBe("$25");
   expect(add).toHaveBeenCalledTimes(1);
+  add.mockImplementation(() => 30);
+  expect(total(5, 20)).toBe("$30")
 });
